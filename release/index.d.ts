ee1c8da4ed1b5cc313698c9295fce532b11bf23d
Cameron Sowder <cameron@sowder.io>
2016-05-16 15:14:12 -0400

    Still testing

--- release/index.d.ts
+++ release/index.d.ts
@@ -1,13 +1,3 @@
-/// <reference path="../typings/main.d.ts" />
-export * from './api/companyAPI';
-export * from './api/expenseAPI';
-export * from './api/financeAPI';
-export * from './api/marketingAPI';
-export * from './api/procurementAPI';
-export * from './api/projectAPI';
-export * from './api/salesAPI';
-export * from './api/scheduleAPI';
-export * from './api/serviceAPI';
-export * from './api/systemAPI';
-export * from './api/timeAPI';
-export * from './connectwise';
+///<reference path="../typings/main.d.ts" />
+///<reference path="./connectwise.d.ts"/>
+export { Connectwise } from './connectwise';
